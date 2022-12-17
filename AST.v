5305d1b8032ecbf46238fe8c27967bdec36b9338
Thalia Archibald <thalia@archibald.dev>
2022-12-17 09:00:39 -0700

    Parse AST

--- AST.v
+++ AST.v
@@ -1,3 +1,6 @@
+Require Import Coq.Lists.List. Import ListNotations.
+Require Import BF.Token.
+
 Inductive ast : Type :=
   | ARight
   | ALeft
@@ -6,3 +9,29 @@ Inductive ast : Type :=
   | AOutput
   | AInput
   | ALoop (body : list ast).
+
+Fixpoint parse_inner (ts : list token) : list ast * list (list ast) :=
+  match ts with
+  | t :: ts' =>
+      match parse_inner ts' with (body, next) =>
+      match t with
+      | TRight => (ARight :: body, next)
+      | TLeft => (ALeft :: body, next)
+      | TInc => (AInc :: body, next)
+      | TDec => (ADec :: body, next)
+      | TOutput => (AOutput :: body, next)
+      | TInput => (AInput :: body, next)
+      | THead => match next with
+                 | next :: next' => (ALoop body :: next, next')
+                 | [] => ([], [[]]) (* unclosed loop *)
+                 end
+      | TTail => ([], body :: next)
+      end end
+  | [] => ([], [])
+  end.
+
+Definition parse (ts : list token) : option (list ast) :=
+  match parse_inner ts with
+  | (prog, []) => Some prog
+  | _ => None
+  end.
