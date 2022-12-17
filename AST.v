b49069d1addd1e3152b9fd55da9ad86041417ca5
Thalia Archibald <thalia@archibald.dev>
2022-12-17 08:32:59 -0800

    Flatten AST to tokens

--- AST.v
+++ AST.v
@@ -2,36 +2,49 @@ Require Import Coq.Lists.List. Import ListNotations.
 Require Import BF.Token.
 
 Inductive ast : Type :=
-  | ARight
-  | ALeft
-  | AInc
-  | ADec
-  | AOutput
-  | AInput
-  | ALoop (body : list ast).
+  | ARight (a : ast)
+  | ALeft (a : ast)
+  | AInc (a : ast)
+  | ADec (a : ast)
+  | AOutput (a : ast)
+  | AInput (a : ast)
+  | ALoop (body : ast) (a : ast)
+  | AEnd.
 
-Fixpoint parse_inner (ts : list token) : list ast * list (list ast) :=
+Fixpoint parse_inner (ts : list token) : ast * list ast :=
   match ts with
   | t :: ts' =>
       match parse_inner ts' with (body, next) =>
       match t with
-      | TRight => (ARight :: body, next)
-      | TLeft => (ALeft :: body, next)
-      | TInc => (AInc :: body, next)
-      | TDec => (ADec :: body, next)
-      | TOutput => (AOutput :: body, next)
-      | TInput => (AInput :: body, next)
+      | TRight => (ARight body, next)
+      | TLeft => (ALeft body, next)
+      | TInc => (AInc body, next)
+      | TDec => (ADec body, next)
+      | TOutput => (AOutput body, next)
+      | TInput => (AInput body, next)
       | THead => match next with
-                 | next :: next' => (ALoop body :: next, next')
-                 | [] => ([], [[]]) (* unclosed loop *)
+                 | next :: next' => (ALoop body next, next')
+                 | [] => (AEnd, [AEnd]) (* unclosed loop *)
                  end
-      | TTail => ([], body :: next)
+      | TTail => (AEnd, body :: next)
       end end
-  | [] => ([], [])
+  | [] => (AEnd, [])
   end.
 
-Definition parse (ts : list token) : option (list ast) :=
+Definition parse (ts : list token) : option ast :=
   match parse_inner ts with
   | (prog, []) => Some prog
   | _ => None
+  end.
+
+Fixpoint flatten (a : ast) : list token :=
+  match a with
+  | ARight a' => TRight :: flatten a'
+  | ALeft a' => TLeft :: flatten a'
+  | AInc a' => TInc :: flatten a'
+  | ADec a' => TDec :: flatten a'
+  | AOutput a' => TOutput :: flatten a'
+  | AInput a' => TInput :: flatten a'
+  | ALoop body a' => THead :: flatten body ++ TTail :: flatten a'
+  | AEnd => []
   end.
