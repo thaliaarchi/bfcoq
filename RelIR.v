fdeaab1928ddd1df425a35f0990aa4ab6928069a
Thalia Archibald <thalia@archibald.dev>
2022-12-27 23:38:05 -0800

    Specify RelIR execute semantics

--- RelIR.v
+++ RelIR.v
@@ -1,12 +1,42 @@
-From BF Require Import Base Byte AST.
+From BF Require Import Base Byte RelVM AST.
 
 Inductive relir : Type :=
-  | RMove (move : Z) (r : relir)
+  | RMove (n : Z) (r : relir)
   | RAdd (n : byte) (off : Z) (r : relir)
   | ROutput (off : Z) (r : relir)
   | RInput (off : Z) (r : relir)
   | RLoop (body : relir) (r : relir)
   | REnd.
+
+Inductive execute : relir -> vm -> vm -> Prop :=
+  | E_RMove : forall n r v v' v'',
+      RelVM.move n v = Some v' ->
+      execute r v' v'' ->
+      execute (RMove n r) v v''
+  | E_RAdd : forall n off r v v' v'',
+      RelVM.add_cell n off v = Some v' ->
+      execute r v' v'' ->
+      execute (RAdd n off r) v v''
+  | E_ROutput : forall off r v v' v'',
+      RelVM.output off v = Some v' ->
+      execute r v' v'' ->
+      execute (ROutput off r) v v''
+  | E_RInput : forall off r v v' v'',
+      RelVM.input off v = Some v' ->
+      execute r v' v'' ->
+      execute (RInput off r) v v''
+  | E_RLoop : forall body r v v' v'',
+      RelVM.cell v =? #00 = false ->
+      execute body v v' ->
+      execute (RLoop body r) v' v'' ->
+      execute (RLoop body r) v v''
+  | E_RLoop_0 : forall body r v v',
+      RelVM.cell v =? #00 = true ->
+      execute r v v' ->
+      execute (RLoop body r) v v'
+  | E_REnd : forall v v',
+      RelVM.eq v v' = true ->
+      execute REnd v v'.
 
 Fixpoint lower_ast (a : ast) : relir :=
   match a with
--- RelVM.v
+++ RelVM.v
@@ -1,5 +1,4 @@
-From BF Require Import Base Byte.
-Require Import Coq.Lists.List.
+From BF Require Import Base Byte VM.
 
 Record vm := VM {
   tape : list byte;
@@ -31,23 +30,6 @@ Definition move (n : Z) (v : vm) : option vm :=
   | None => None
   end.
 
-Fixpoint extend {A : Type} (n : nat) (l : list A) (default : A) : list A :=
-  match n with
-  | O => l
-  | S n' => match l with
-            | [] => default :: extend n' [] default
-            | h :: l' => h :: extend n' l' default
-            end
-  end.
-
-Lemma extend_length : forall A n (l : list A) x,
-  n <= length (extend n l x).
-Proof.
-  induction n; intros.
-  - apply Nat.le_0_l.
-  - destruct l; cbn; rewrite <- Nat.succ_le_mono; apply IHn.
-Qed.
-
 Fixpoint list_map_nth {A : Type} (f : A -> A) (default : A) (n : nat) (l : list A) : list A :=
   match n, l with
   | O, [] => [f default]
@@ -56,5 +38,39 @@ Fixpoint list_map_nth {A : Type} (f : A -> A) (default : A) (n : nat) (l : list 
   | S n', h :: l' => h :: list_map_nth f default n' l'
   end.
 
-Definition tape_map_nth (f : byte -> byte) (n : nat) (v : vm) : vm :=
-  set_tape (list_map_nth f #00 n v.(tape)) v.
+Definition map_cell (f : byte -> byte) (off : Z) (v : vm) : option vm :=
+  match ptr_rel off v with
+  | Some p => Some (set_tape (list_map_nth f #00 (N.to_nat p) v.(tape)) v)
+  | None => None
+  end.
+
+Definition cell (v : vm) : byte :=
+  nth (N.to_nat v.(ptr)) v.(tape) #00.
+
+Definition cell_at (off : Z) (v : vm) : option byte :=
+  match ptr_rel off v with
+  | Some p => Some (nth (N.to_nat p) v.(tape) #00)
+  | None => None
+  end.
+
+Definition set_cell (n : byte) := map_cell (fun _ => n).
+
+Definition add_cell (n : byte) := map_cell (fun c => c + n).
+
+Definition output (off : Z) (v : vm) : option vm :=
+  match cell_at off v with
+  | Some c => Some (set_outputs (c :: v.(outputs)) v)
+  | None => None
+  end.
+
+Definition input (off : Z) (v : vm) : option vm :=
+  match v.(inputs) with
+  | h :: i' => set_cell h off (set_inputs i' v)
+  | [] => None
+  end.
+
+Definition eq (v1 v2 : vm) : bool :=
+  let (t1, p1, o1, i1) := v1 in
+  let (t2, p2, o2, i2) := v2 in
+  VM.list_byte_eq t1 t2 && (p1 =? p2)%N
+    && VM.list_byte_eq o1 o2 && VM.list_byte_eq i1 i2.
